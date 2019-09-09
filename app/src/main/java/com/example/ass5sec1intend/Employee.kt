5acf2f588b4f15bc56ab95bef56b12b48a169a06
wanjaengp <54529778+wanjaengp@users.noreply.github.com>
2019-09-09 16:20:01 +0700

    Ass5_Sec1_603021729_7

---
+++ app/src/main/java/com/example/ass5sec1intend/Employee.kt
@@ -0,0 +1,12 @@
+package com.example.ass5sec1intend
+
+
+
+import  android.os.Parcelable
+import  kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Employee (val name: String, val gender:String, val email: String, val salary: Int) : Parcelable{
+
+
+}
